bf018877a85f6f89734bea9feb42902e03fe4d51
Nrepesh <nrepesh.joshi@gmail.com>
2020-02-04 12:56:21 -0600

    The slicing trail effect
    Added a slicing object and added a trail to it

---
+++ CS292-Template/Assets/Scripts/Blade.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blade : MonoBehaviour
+{
+    bool isCutting = false;
+    Rigidbody2D rb;
+    Camera c;
+
+    private void Start()
+    {
+        c = Camera.main;
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCutting();
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            StopCutting();
+        }
+
+        if (isCutting)
+        {
+            UpdateCutting();
+        }
+    }
+
+    void UpdateCutting()
+    {
+        rb.position = c.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void StartCutting()
+    {
+        isCutting = true;
+    }
+
+    void StopCutting()
+    {
+        isCutting = false;
+    }
+}
