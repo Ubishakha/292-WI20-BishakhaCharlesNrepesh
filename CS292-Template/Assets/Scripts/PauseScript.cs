8db46b4fe9462fa73c1543debea103a505e07c72
Charles Broomfield <ccbroomfield@knox.edu>
2020-02-05 21:02:15 -0600

    Made objects disappear when they fall off screen

---
+++ CS292-Template/Assets/Scripts/PauseScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    // Update is called once per frame
+    public GameObject pausebutton;
+
+    // public static GameObject selectedBtn;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)){
+            if (GameIsPaused){
+                Resume();
+            }
+            else{
+                PauseIt();
+            }
+        }
+    }
+    public void Resume(){
+        pausebutton.SetActive(false);
+        Time.timeScale =1f;
+        GameIsPaused = false;
+    }
+
+    void PauseIt(){
+        pausebutton.SetActive(true);
+        Time.timeScale =0f;
+        GameIsPaused = true;
+    }
+    
+}
