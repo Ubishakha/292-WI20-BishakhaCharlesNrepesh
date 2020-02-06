c3635e96d7eb89e578f7c8b396f4d15f0c791442
Bishakha Upadhyaya <bupadhyaya@knox.edu>
2020-02-05 22:23:29 -0600

    Pause and resume fixes
    Resume button works properly now

--- CS292-Template/Assets/Scripts/PauseScript.cs
+++ CS292-Template/Assets/Scripts/PauseScript.cs
@@ -6,28 +6,26 @@ public class PauseScript : MonoBehaviour
 {
     public static bool GameIsPaused = false;
     // Update is called once per frame
-    public GameObject pausebutton;
+    public GameObject pausePanel;
 
     // public static GameObject selectedBtn;
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)){
-            if (GameIsPaused){
-                Resume();
-            }
-            else{
-                PauseIt();
-            }
+        if (pausePanel.activeSelf == true){
+            PauseIt();
+        }
+        else{
+            Resume();
         }
     }
     public void Resume(){
-        pausebutton.SetActive(false);
+        pausePanel.SetActive(false);
         Time.timeScale =1f;
         GameIsPaused = false;
     }
 
     void PauseIt(){
-        pausebutton.SetActive(true);
+        pausePanel.SetActive(true);
         Time.timeScale =0f;
         GameIsPaused = true;
     }
