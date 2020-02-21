dae5aa18d13284beb6b5a3082398823cfd899416
Nrepesh <nrepesh.joshi@gmail.com>
2020-02-20 18:23:23 -0600

    knox loses lives 2
    Knox loses lives and clean scripts

--- CS292-Template/Assets/Scripts/LogosKnox.cs
+++ CS292-Template/Assets/Scripts/LogosKnox.cs
@@ -3,17 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Logos2 : MonoBehaviour
+public class LogosKnox : MonoBehaviour
 {
-   // public GameObject explosion;
+    // public GameObject explosion;
+    public AudioSource hitTrigger;
 
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.tag == "Blade")
         {
+            hitTrigger = GameObject.FindGameObjectWithTag("ground").GetComponent<AudioSource>();
             //Instantiate(explosion, transform.position, Quaternion.identity);
             Destroy(gameObject);
+            LivesControl.health -= 1;
+            hitTrigger.Play();
         }
-        Destroy(gameObject,2f);
+        Destroy(gameObject, 2f);
     }
 }
